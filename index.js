29c2b05832c9b44f6586fc9ee8139ab606d4e9bd
Sharon Mwenda <smkathambi@gmail.com>
2022-06-04 11:10:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+/*function Arrayfunctions(){
+function beforeEach() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}*/
+
+function destructivelyAppendCat(name) {
+    cats.push(name); 
+} {
+destructivelyAppendCat("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+destructivelyPrependCat("Bob");
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+destructivelyRemoveLastCat();
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+destructivelyRemoveFirstCat();
+ 
+function appendCat(name) {
+    let newCats = [...cats, name];
+    return newCats;
+}
+appendCat("Broom");
+
+function prependCat(name) {
+    let preCats = [name, ...cats];
+    return preCats;
+}
+prependCat("Arnold");
+
+function removeLastCat() {
+    let lastCats = cats.slice(0, cats.length - 1);
+    return lastCats;
+}
+removeLastCat();
+
+function removeFirstCat() {
+    let firstCats = cats.slice(1);
+    return firstCats;
+}
+removeFirstCat;
